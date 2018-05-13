12ed947f76f59ccd13b1ee57e08f8c361ebccd17
Matthew Hatcher <matthewh@halfpetal.com>
2018-05-13 19:51:58 -0400

    Addiong some more functions to the helper.

--- src/lib/Helpers.ts
+++ src/lib/Helpers.ts
@@ -1,3 +1,15 @@
 export class Helper {
     constructor(public commandPrefix: string) {}
+
+    public getCommand(input: string): string {
+        return input.replace(this.commandPrefix, "").split(" ")[0];
+    }
+
+    public isCommand(input: string): boolean {
+        return (input.toLowerCase().substr(0, this.commandPrefix.length) === this.commandPrefix);
+    }
+
+    public isNumber(input: any): boolean {
+        return !isNaN(parseFloat(input)) && isFinite(input);    
+    }
 }
