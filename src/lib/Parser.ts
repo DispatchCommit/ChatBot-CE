43445d1ad19110afbfdf8aa797a9e9ff97f1a649
Matthew Hatcher <matthewh@halfpetal.com>
2018-05-13 21:11:36 -0400

    Oops. Didn't add all of them.

--- src/lib/Parser.ts
+++ src/lib/Parser.ts
@@ -1,10 +1,38 @@
+import { Helper as BotHelper } from "./Helpers";
+import * as Ora from "ora";
+import * as fs from "fs";
+import * as path from "path";
+import chalk from "chalk";
+
+export class Parser {
+    public helper: BotHelper;
+    public commands = {};
+
+    constructor() {
+        this.helper = new BotHelper(process.env.COMMAND_PREFIX);
+        new Ora("Bot helpers initiated and ready to go!").succeed();
+
+        this.loadAddons();
+    }
+
     public parse(message: Message) {
         
     }
+
+    private loadAddons(): void {
         //let addonSpinner = new Ora("Loading bot addons.").start();
         let addonFolders = this.getFolders(process.env.ADDON_FOLDER);
         console.log(process.env.ADDON_FOLDER);
         console.log(addonFolders);
+    }
+
+    private getFolders(folderPath: string) {
+        return fs.readdirSync(folderPath).filter((file) => {
+            return fs.statSync(path.join(folderPath, file)).isDirectory();
+        });
+    }
+}
+
 interface Message {
 
 }
