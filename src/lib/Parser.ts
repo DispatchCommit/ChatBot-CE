3671520aa9d5fb9c7819dd621df5c43edb328a37
Matthew Hatcher <matthewh@halfpetal.com>
2018-05-13 21:11:20 -0400

    Adding base Parser.

---
+++ src/lib/Parser.ts
@@ -0,0 +1,10 @@
+    public parse(message: Message) {
+        
+    }
+        //let addonSpinner = new Ora("Loading bot addons.").start();
+        let addonFolders = this.getFolders(process.env.ADDON_FOLDER);
+        console.log(process.env.ADDON_FOLDER);
+        console.log(addonFolders);
+interface Message {
+
+}
